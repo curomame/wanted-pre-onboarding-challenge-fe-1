643cf1483c7deadf488a2dddf44b23ab513e5a0d
curomame <curomame@naver.com>
2023-01-04 15:51:40 +0900

    feat : router setting
    경로별 라우터 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/SignUp";
+import Index from "./components/Index";
+
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/singup" element={<Signup />} />
+      </Routes>
+    </>
   );
 }
 
